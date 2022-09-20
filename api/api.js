8052586c765309b7b97207fffc87f113b4582a24
Mikeal <mwiemszra@gmail.com>
2022-09-20 20:48:10 +0200

    api database connect

--- api/api.js
+++ api/api.js
@@ -1,6 +1,24 @@
 const router=require('express').Router();
-const mongodb=require('mongodb').MongoClient;
+const { MongoClient }=require('mongodb');
 const bcrypt=require('bcrypt');
 const jwt=require('jsonwebtoken');
-const verify=require('./middleware/verifytoken');
+const { verifytoken }=require('./middleware/verifytoken');
 module.exports=router;
+
+// Connect to DB
+const connect = async () => {
+    const uri='mongodb://127.0.0.1:27017';
+    const dbname='Chaos';
+    //mongodb://[username:password@]host1[:port1][,...hostN[:portN]][/[defaultauthdb][?options]]
+    const client=new MongoClient(uri, {});
+    try {
+        await client.connect();
+        await client.db(dbname).command({ping: 1});
+    } catch(e) {
+        console.error('\x1b[31m','Database error:','\x1b[0m',e);
+    } finally {
+        console.log('\x1b[32m','Server connected','\x1b[0m',`(on url ${uri})...`);
+        await client.close();
+    }
+};
+connect();
--- api/middleware/verifytoken.js
+++ api/middleware/verifytoken.js
@@ -1,1 +1,16 @@
 const jwt=require('jsonwebtoken');
+
+const verifytoken=(req, res, next) => {
+    const token=req.header('auth-token');
+    if(!token) {return res.status(401).send('Acces Denied');}
+
+    try {
+        const verified=jwt.verify(token, process.env.TOKEN_SECRET);
+        req.user=verified;
+        next();
+    }
+    catch(err) {
+        res.status(400).send('Invalid token');
+    }
+};
+module.module.exports=verifytoken;
