53a9a481de2629c483c2da5c436f7b4f88fd4580
Mikeal <mwiemszra@gmail.com>
2022-09-25 21:57:29 +0200

    website uppdate

--- web/web.js
+++ web/web.js
@@ -4,3 +4,11 @@ module.exports=router;
 router.get('/', (req, res) => {
     res.render('landing', {});
 });
+
+router.get('/signin', (req, res) => {
+    res.render('sign_in', {});
+});
+
+router.get('/signup', (req, res) => {
+    res.render('sign_up', {});
+});
