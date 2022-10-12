13957cd6ff40ce86bf27b0729522c4997f607123
Mikeal <mwiemszra@gmail.com>
2022-10-12 20:07:48 +0200

    dashboard link and /test

--- web/web.js
+++ web/web.js
@@ -1,6 +1,8 @@
 const router=require('express').Router();
+const verifytoken=require('../api/middlewares/verifytoken');
 module.exports=router;
 
+// Public routes
 router.get('/', (req, res) => {
     res.render('landing', {});
 });
@@ -12,3 +14,11 @@ router.get('/sign_in', (req, res) => {
 router.get('/sign_up', (req, res) => {
     res.render('sign_up', {});
 });
+
+// Private routes
+router.get('/chaos', verifytoken, (req, res) => {
+    res.render('dashboard', {});
+});
+
+/* Test */
+router.get('/test', (req, res) => {res.render('dashboard')});
